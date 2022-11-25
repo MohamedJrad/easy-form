b6ef916eb1cacce468d39f9f90a3fd3ea12e54c0
Mohamed Jrad <mohamed.jrad@proton.me>
2022-11-25 23:05:16 +0100

    feat(setup): update

--- packages/react-form-fields/src/fields/CheckboxGroup/types.ts
+++ packages/react-form-fields/src/fields/CheckboxGroup/types.ts
@@ -1,26 +1,26 @@
 export interface Option {
-  value: string
-  label: string
+  value: string;
+  label: string;
 }
 
 export interface Props {
-  name: string
-  label: string
-  defaultValue?: string
-  isHidden?: boolean
-  isReadOnly?: boolean
-  isRequired?: boolean
-  isTouched?: boolean
-  isDisabled?: boolean
-  error?: string
-  isWithRequiredStar?: boolean
-  options?: Option[]
-  onValueChange?: (e: string[]) => void
-  direction: 'horizontal' | 'vertical'
+  name: string;
+  label: string;
+  defaultValue?: string;
+  isHidden?: boolean;
+  isReadOnly?: boolean;
+  isRequired?: boolean;
+  isTouched?: boolean;
+  isDisabled?: boolean;
+  error?: string;
+  isWithRequiredStar?: boolean;
+  options?: Option[];
+  onValueChange?: (e: string[]) => void;
+  direction: "horizontal" | "vertical";
 }
 
 export interface ICheckbox {
-  label: string
-  value: string
-  isChecked: boolean
+  label: string;
+  value: string;
+  isChecked: boolean;
 }
